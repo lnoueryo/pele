b2f2ed70299f14a3eb9b93348768e9b45cef3514
lnoueryo <inoue@withlive.jp>
2025-01-19 01:45:22 +0900

    feat: webpackからviteに変更

--- src/main.ts
+++ src/main.ts
@@ -67,7 +67,7 @@ export const onChangeControllerPositionClicked = (e) => {
 
 let timer = null;
 let controller = null;
-
+console.log(controller)
 const canvas = document.getElementById('game-canvas') as HTMLCanvasElement;
 const ctx = canvas.getContext('2d');
 
